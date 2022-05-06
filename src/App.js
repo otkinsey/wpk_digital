306d53a6a78c3b904967716822ae954deb9bf95e
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2022-05-06 15:54:23 -0400

    homepage scrolling working as expected. must add contact form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { ReactComponent as Logo } from "./components/header/logo.svg";
 import "./App.css";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./components/homePage";
 
+import { ReactComponent as Logo } from "./components/header/logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
+  const scrollTo = (id) => {
+    const elem = document.getElementById(id.substring(id.indexOf("_") + 1));
+    console.log(elem);
+    elem.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="App">
       <header
@@ -16,6 +20,7 @@ function App() {
 
           margin: "none",
         }}
+        id="header"
       >
         <ul
           style={{
@@ -32,17 +37,59 @@ function App() {
           <li>
             <Logo />
           </li>
-          <li>ELEARNING</li>
-          <li>DOCUMENT MANAGEMENT</li>
-          <li>DIGITALTRANSFORMATION</li>
-          <li>CONNECT WITH US</li>
+          <li
+            onClick={(event) => {
+              scrollTo(event.target.id);
+            }}
+            id="nav_elearning"
+          >
+            ELEARNING
+          </li>
+          <li
+            onClick={(event) => {
+              scrollTo(event.target.id);
+            }}
+            id="nav_document_management"
+          >
+            DOCUMENT MANAGEMENT
+          </li>
+          <li
+            onClick={(event) => {
+              scrollTo(event.target.id);
+            }}
+            id="nav_digital_transformation"
+          >
+            DIGITALTRANSFORMATION
+          </li>
+          <li
+            onClick={(event) => {
+              scrollTo(event.target.id);
+            }}
+            id="nav_contact_us"
+          >
+            CONNECT WITH US
+          </li>
         </ul>
       </header>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home scrollTo={scrollTo} />} />
         </Routes>
       </Router>
+      <footer
+        style={{
+          background: "black",
+          height: "150px",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "0 30%",
+        }}
+      >
+        <Logo style={{ height: 45 }} />
+        <p>&copy; WPK Digital Solution copyright 2022. All rights reserved</p>
+      </footer>
     </div>
   );
 }
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,7 +1,9 @@
-const HomePage = () => {
+import { BsChevronUp } from "react-icons/bs";
+
+const HomePage = (props) => {
   const pageData = [
     {
-      sectionID: "jumbotron",
+      sectionID: "hero",
       title: "Lorem ipsum dolor ",
       text: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. QuLorem ipsum doloris ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
       imageURL: "images/homepage/tyler-franta-RbFDzMKTH6Q-unsplash.png",
@@ -27,15 +29,16 @@ const HomePage = () => {
   ];
 
   const createSectionContent = (section, index) => {
-    console.log("section id: ", section.sectionID, index);
     const alignment = index % 2 === 0 ? "left" : "right";
     const textColor = index % 2 === 0 ? "black" : "white";
-    const textPosition = index % 2 === 0 ? "3%" : "67%";
+    const textPosition = index % 2 === 0 ? "10%" : "57%";
     return (
       <div
         className="section"
         id={section.sectionID}
-        style={{ background: `center/cover url(${section.imageURL})` }}
+        style={{
+          background: `center/100% no-repeat url(${section.imageURL})`,
+        }}
       >
         <div
           className="section_text"
@@ -44,23 +47,7 @@ const HomePage = () => {
           <h1>{section.title.replace("_", " ")}</h1>
           <p>{section.text}</p>
           {index === 0 ? (
-            <a
-              href="/"
-              style={{
-                display: "block",
-                backgroundColor: "orange",
-                zIndex: "1",
-                position: "relative",
-                color: "white",
-                textAlign: "center",
-                fontSize: "1rem",
-                fontWeight: "bold",
-                padding: "15px 20px",
-                width: "120px",
-                textDecoration: "none",
-                borderRadius: "8px",
-              }}
-            >
+            <a href="/" className="btn-primary" style={{}}>
               Contact Us
             </a>
           ) : (
@@ -74,6 +61,28 @@ const HomePage = () => {
   return (
     <div>
       {pageData.map((section, index) => createSectionContent(section, index))}
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          props.scrollTo(event.target.id);
+        }}
+        id="button_header"
+        style={{
+          height: 40,
+          width: 40,
+          borderRadius: 20,
+          backgroundColor: "rgba(0,0,0,.45)",
+          color: "white",
+          fontSize: "2rem",
+          border: "1px solid #fff",
+          position: "fixed",
+          right: "3%",
+          bottom: "6%",
+          boxShadow: "0 0 10px #fff",
+        }}
+      >
+        <BsChevronUp />
+      </button>
     </div>
   );
 };
