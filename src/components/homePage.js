75af031bf779a78cd9ba829eae226b3593cd42ed
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2022-08-04 19:02:18 -0400

    starting responsive

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -24,7 +24,7 @@ const HomePage = (props) => {
     },
     {
       sectionID: "digital_transformation",
-      title: "Digital transformation",
+      title: "Digital Transformation",
       text: "Step, boldly into the future. Digital transformation consulting services, from WPK, will help your organization  identify opportunities, formulate strategy, and remove impediments. Deliver your products with increased speed and reduced cost, with WPK.",
       imageURL: "images/homepage/digital_transformaton.png",
     },
@@ -32,20 +32,17 @@ const HomePage = (props) => {
 
   const createSectionContent = (section, index) => {
     const alignment = index % 2 === 0 ? "left" : "right";
-    const textColor = index % 2 === 0 ? "#6c6c6c" : "white";
+    const textColor = index % 2 === 0 ? "6c" : "white";
     const textPosition = index % 2 === 0 ? "7%" : "71%";
     return (
       <div
         className="section"
         id={section.sectionID}
         style={{
-          background: `center/100% no-repeat url(${section.imageURL})`,
+          backgroundImage: `url(${section.imageURL})`,
         }}
       >
-        <div
-          className="section_text"
-          style={{ textAlign: alignment, left: textPosition, color: textColor }}
-        >
+        <div className={`section_text ${alignment}`} style={{}}>
           <h1>{section.title.replace("_", " ")}</h1>
           <p>{section.text}</p>
           {index === 0 ? (
@@ -67,7 +64,63 @@ const HomePage = (props) => {
     );
   };
 
-  return <h1>Currently undergoing maintenance. Thank you for visiting.</h1>;
+  return (
+    <div>
+      <div id="main-content">
+        {pageData.map((section, index) => createSectionContent(section, index))}
+        <div className="section" id="contact_us" style={{ color: "#6c6c6c" }}>
+          <Form>
+            <h1>We'd love to hear from you.</h1>
+            <Row>
+              <Col>
+                <label>First Name:</label>
+                <Form.Control />
+              </Col>
+              <Col>
+                <label>Last Name:</label>
+                <Form.Control />
+              </Col>
+            </Row>
+            <Row>
+              <label>Company Name:</label>
+              <Form.Control></Form.Control>
+            </Row>
+            <Row>
+              <label>Email:</label>
+              <Form.Control></Form.Control>
+            </Row>
+            <Button type="submit" className="button">
+              <FaPaperPlane style={{}} /> <span>Send</span>
+            </Button>
+          </Form>
+        </div>
+      </div>
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          props.scrollTo(event.target.id);
+        }}
+        id="button_header"
+        style={{
+          height: 40,
+          width: 40,
+          borderRadius: 20,
+          backgroundColor: "rgba(0,0,0,.45)",
+          color: "white",
+          fontSize: "2rem",
+          border: "1px solid #fff",
+          position: "fixed",
+          right: "3%",
+          bottom: "6%",
+          boxShadow: "0 0 10px #fff",
+        }}
+      >
+        <BsChevronUp />
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
+
+// 617-730-2069
