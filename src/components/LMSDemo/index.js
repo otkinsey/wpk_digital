dc8d09c99fab8c3d2367f695bc1d94858087f9f3
okoa kinsey <okoakinsey@okoas-MBP.lan>
2023-01-02 15:07:20 -0500

    formatting demo-interface

--- src/components/LMSDemo/index.js
+++ src/components/LMSDemo/index.js
@@ -5,14 +5,35 @@ import React from "react";
 class LMSDemo extends React.Component {
   render() {
     return (
-      <div id="demo" className={`demo ${this.props.LMSActive ? "active" : ""}`}>
-        <button
-          className="button btn-danger"
-          onClick={() => this.props.toggleDemoLMS()}
-        >
-          End demo
-        </button>
-        <h1>Demo</h1>
+      <div
+        id="demo"
+        className={`container-fluid slide-down-panel ${
+          this.props.LMSActive ? "active" : ""
+        }`}
+      >
+        <div class="row">
+          <div class="col-sm toggle-row" id="">
+            <button
+              className="button btn-danger"
+              onClick={() => this.props.toggleDemoLMS()}
+            >
+              End demo
+            </button>
+          </div>
+        </div>
+        <div class="row demo-interface" id="">
+          <div class="col col-3">
+            <div class="wrapper">
+              <h1>Demo</h1>
+              <h3>column 1</h3>
+            </div>
+          </div>
+          <div class="col col-9">
+            <div class="wrapper">
+              <h3>col 2</h3>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
